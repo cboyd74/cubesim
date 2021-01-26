5481bb8971d5ee6d54f6ad13fae8d7674c99d128
cboyd74 <crboyd@wisc.edu>
2021-01-25 18:04:58 -0600

    change strategy for make_move, needs lots of work

--- cubesim.py
+++ cubesim.py
@@ -34,7 +34,19 @@ class Cube:
 		self.right = Side(3, self.size)
 		self.top = Side(4, self.size)
 		self.bottom = Side(5, self.size)
-	
+		
+		self.front.init_borders(self.top.values[self.size-1, :].copy(), self.left.values[:, self.size-1].copy(), self.right.values[:, 0].copy(), self.bottom.values[0, :].copy())
+
+		self.back.init_borders(np.flip(self.top.values[self.size-1, :].copy()), self.right.values[:, self.size-1].copy(), self.left.values[:, 0].copy(), np.flip(self.bottom.values[self.size-1, :].copy()))
+
+		self.left.init_borders(self.top.values[0, :].copy(), self.back.values[:, self.size-1], self.front.values[:, 0], np.flip(self.bottom.values[:, 0].copy()))
+
+		self.right.init_borders(np.flip(self.top.values[:, self.size-1].copy()), self.front.values[:, self.size-1].copy(), self.back.values[:, 0].copy(), self.bottom.values[:, self.size-1].copy())
+
+		self.top.init_borders(np.flip(self.back.values[0, :].copy()), self.left.values[0,:], np.flip(self.right.values[0, :].copy()), self.front.values[0,:])
+
+		self.bottom.init_borders(self.front.values[self.size-1,:].copy(), np.flip(self.left.values[self.size-1,:].copy()), self.right.values[self.size-1, :].copy(), np.flip(self.back.values[self.size-1,:].copy()))
+
 	# Reorients the cube in the given direction: up, down, left, right
 	def orient(self, direction):
 
@@ -57,53 +69,53 @@ class Cube:
 			tmp = self.back
 			tmp.rotate_right()
 			tmp.rotate_right()
-			self.back = self.bottom # bottom becomes back
+			self.back = self.bottom
 			self.back.rotate_right()
 			self.back.rotate_right()
 			tmp2 = self.top
-			self.top = tmp # back becomes top
+			self.top = tmp
 			tmp = self.front
-			self.front = tmp2 # top becomes front
-			self.bottom = tmp # front becomes bottom
-			self.left.rotate_right() # rotate left side
-			self.right.rotate_left() # rotate right side
+			self.front = tmp2
+			self.bottom = tmp
+			self.left.rotate_right()
+			self.right.rotate_left()
 
-		elif(direction == "left"): # move cube left
+		elif(direction == "left"): 
 			tmp = self.front
-			self.front = self.right # right becomes front
+			self.front = self.right 
 			tmp2 = self.left
-			self.left = tmp # front becomes left
+			self.left = tmp
 			tmp = self.back
-			self.back = tmp2 # left becomes back
-			self.right = tmp # back becomes right
-			self.top.rotate_right()# rotate top 
-			self.bottom.rotate_left() # rotate bottom
+			self.back = tmp2 
+			self.right = tmp 
+			self.top.rotate_right() 
+			self.bottom.rotate_left()
 
-		elif(direction == "right"): #move cube right
+		elif(direction == "right"):
 			tmp = self.front	
-			self.front = self.left # left becomes front
+			self.front = self.left
 			tmp2 = self.right
-			self.right = tmp # front becomes right 
+			self.right = tmp  
 			tmp = self.back
-			self.back = tmp2 # right becomes back
-			self.left = tmp # back becomes left
-			self.top.rotate_left() # rotate top 
-			self.bottom.rotate_right() # rotate bottom
+			self.back = tmp2 
+			self.left = tmp 
+			self.top.rotate_left() 
+			self.bottom.rotate_right()
 
 		elif (direction == "side left"):
 			tmp = self.top
 			tmp.rotate_left() 
-			self.top = self.right # top becomes right
+			self.top = self.right 
 			self.top.rotate_left()
 			tmp2 = self.left
 			tmp2.rotate_left() 
-			self.left = tmp # left becomes top
+			self.left = tmp 
 			tmp = self.bottom 
 			tmp.rotate_left()
-			self.bottom = tmp2 # bottom becomes left
-			self.right = tmp # right becomes bottom
-			self.front.rotate_left() # rotate front
-			self.back.rotate_right() # rotate back	
+			self.bottom = tmp2 
+			self.right = tmp 
+			self.front.rotate_left() 
+			self.back.rotate_right()
 
 		elif (direction == "side right"):
 			tmp = self.top
@@ -114,11 +126,11 @@ class Cube:
 			tmp2.rotate_right()
 			self.right = tmp
 			tmp = self.bottom
-			tmp.rotate_right() #rotate right to adjust layout
+			tmp.rotate_right() 
 			self.bottom = tmp2
 			self.left = tmp
-			self.front.rotate_right() # rotate front
-			self.back.rotate_left() # rotate back
+			self.front.rotate_right()
+			self.back.rotate_left()
 
 		else: # input error
 			print("orient_cube: input error!")
@@ -128,44 +140,31 @@ class Cube:
 		face.rotate_right()
 
         #top's last row becomes first column of right (in order)
-		tmp = []
-		for i in range(0, self.size):
-			tmp.append(right.values[i][0]) #save right's first column
-			right.values[i][0] = top.values[self.size-1][i] #set right's first column
-
+		tmp = face.right_border.copy()
+		face.right_border = face.top_border
         #first column of right becomes first row of bottom (in reverse)
-		tmp2 = []
-		j = self.size-1 
-		for i in range(0, self.size):
-			tmp2.append(bottom.values[0][i]) #save bottom's first  
-			bottom.values[0][i] = tmp[j] #set bottom's first column (reverse)
-			j-=1
-
+		tmp2 = face.bottom_border.copy() 
+		face.bottom_border = np.flip(tmp)
         #first row of bottom becomes last column of left (in order)
-		tmp = []
-		for i in range(0, self.size):
-			tmp.append(left.values[i][self.size-1]) #save left's last column 
-			left.values[i][self.size-1] = tmp2[i] #set left's first column (reverse)
-
+		tmp = face.left_border.copy()
+		face.left_border = tmp2 
         #last column of left becomes last row of top (in reverse)
-		j = self.size-1
+		face.top_border = np.flip(tmp)
+		#Update side values
 		for i in range(0, self.size):
-			top.values[self.size-1][i] = tmp[j]
-			j-=1
+			top.values[self.size-1][i] = face.top_border[i]
+			left.values[i][self.size-1] = face.left_border[i]
+			right.values[i][0] = face.right_border[i]
+			bottom.values[0][i] = face.bottom_border[i]
 
 	def prime(self, face, top, bottom, left, right):
 		#rotate front left
 		face.rotate_left()
-
         #top's last row becomes last column of left (in reverse)
-
         #left's last column becomes bottom's first row (in order)
-
         #bottom's first row becomes right's first column (in reverse)
-
         #right's first column becomes top's bottom row (in order)
 
-	#peforms the specified move, changes state of cube	
 	def make_move(self, side, p):
 		print("")
 		
@@ -215,14 +214,12 @@ class Cube:
 	def print_cube(self):
 		print("- - - - - - - - - - - - - - - - - - - - - -")
 		print(" - - - - - - - - - - - - - - - - - - - - -")
-
 		#print top
 		for i in range(0, self.size):
 			for h in range(0, self.size+8): #for spacing
 				print(" ", sep = "", end = "")
 			self.print_row(self.top, i)
 			print("")
-
 		#print sides 2, then 0, then 3, then 1
 		for i in range(0, self.size):
 			print("   ", sep = "", end = "")
@@ -234,33 +231,40 @@ class Cube:
 			print("    ", end = "")
 			self.print_row(self.back, i)
 			print("")
-
 		#print side 5 on bottom		
 		for i in range(0, self.size):
 			for h in range(0, self.size+8):
 				print(" ", sep = "", end = "")
 			self.print_row(self.bottom, i)
 			print("")
-
 		print(" - - - - - - - - - - - - - - - - - - - - -")
 		print("- - - - - - - - - - - - - - - - - - - - - -")
 
 class Side:
 
 	# members
-	#  |--	values:    2D numpy matrix	
-	#  |--	id:        ID of this side (0 - 5)
-	#  |--  neighbors: 
-	#
+	#  |--	values:          2D numpy matrix	
+	#  |--	id:              ID of this side (0 - 5)
+	#  |--  top_border:      numpy array of values bordering top
+	#  |--  left_border:     
+	#  |--  right_border:
+	#  |--  bottom_border:  
+
 	def __init__(self, id, size):
 		self.size = size
 		self.id = id
 		self.values = np.full((self.size, self.size), self.id)
+		self.top_border = np.zeros(self.size) 
+		self.left_border = np.zeros(self.size) 
+		self.right_border = np.zeros(self.size) 
+		self.bottom_border = np.zeros(self.size) 
 
-	# Function to rotate the values of the cube
-	#  either left 90 or right 90
-	#  1 2   -->  3 1       1 2  -->  2 4
-	#  3 4        4 2   or  3 4       1 3
+	def init_borders(self, top, left, right, bottom):
+		self.top_border = top
+		self.left_border = left
+		self.right_border = right
+		self.bottom_border = bottom
+	
 	def rotate_right(self):
 		new_values = np.full((self.size, self.size), self.values)
 		row = 0
@@ -303,7 +307,6 @@ class Sim:
 			inp = input()
 			if (inp == "Q" or inp == "q"):
 				break
-
 			if (len(inp) > 3): #error if command has more than 2 characters
 				print("Error: command not recognized")
 			else:	
@@ -333,5 +336,4 @@ class Sim:
 					cube.make_move("b", 0)
 				else:
 					print("Error: command not recognized")
-
 			cube.print_cube()
